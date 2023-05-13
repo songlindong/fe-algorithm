92ddbeee6ed813be1e3ea15947256987ff65d594
songlindong <857344690@qq.com>
2023-05-13 11:17:03 +0800

    feat: 了解算法复杂度

---
+++ 了解算法复杂度/了解算法复杂度.js
@@ -0,0 +1,34 @@
+// leetcode 1. 两数之和
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// // 复杂度
+//   数组长度是n: 算法大概执行了多少次，时间复杂度是O(n^2)
+//   空间复杂度：i和j都是一个普通的数字O(1)
+
+// 数组长度是n,时间复杂度On
+// 空间复杂度 O(n)
+var twoSum = function(nums, target) {
+    // for(let i=0; i<nums.length; i++){ 
+    //     console.log('i==' + i)
+    //     for(let j=0; j<nums.length; j++){
+    //         console.log('j==' + j)
+    //         if(nums[i] + nums[j]===target && i!==j){
+    //             return [i, j]
+    //         }
+    //     }
+    // }
+    let obj = {}
+    for(let i=0; i<nums.length; i++){
+        let num = nums[i]
+        let n = target - num
+        if(num in obj){
+          return [i, obj[num]]
+        }else{
+            obj[n] = i
+        }
+    }
+
+};
